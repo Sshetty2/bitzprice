e52fe2429840aa0f6b6903760b216d8840e9eb3b
Sachit <sachit.shetty@gmail.com>
2019-02-20 05:57:32 -0500

    add in login screen components and app bar and menu as a test

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,23 @@
-import Fetch from 'isomorphic-unfetch';
 import Layout from '../components/Layout';
-
 import Prices from '../components/Prices';
+import Login from '../components/LoginPage';
+import App from '../components/App'
+import "isomorphic-fetch"
 
-const Index = (props) => (
-  <Layout>
-    <div>
-      <h1>Welcome to BitzPrice</h1>
-      <p>Check current Bitcoin rate</p>
-      <Prices bpi={props.bpi}/>
-    </div>
-  </Layout>
+
+// const Index = (props) => (
+//   <Layout>
+//     <div>
+//       <h1>Welcome to BitzPrice</h1>
+//       <p>Check current Bitcoin rate</p>
+//       <Prices bpi={props.bpi}/>
+//     </div>
+//   </Layout>
+// );
+
+
+const Index = (props) => ( 
+  <App />
 );
 
 Index.getInitialProps = async function() {
