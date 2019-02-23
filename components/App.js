fb6fa8dc929645a7daedeec65960ff3f108ce657
Sachit <sachit.shetty@gmail.com>
2019-02-23 10:26:58 -0500

    start integrating material ui configurations to render material ui components with styles

--- components/App.js
+++ components/App.js
@@ -4,11 +4,27 @@ import Prices from './Prices';
 import LoginScreen from './Loginscreen';
 import TweetsPanels from './TweetPanels'
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+import JssProvider from 'react-jss/lib/JssProvider';
+import getPageContext from '../src/getPageContext';
+
 class App extends Component {
   constructor(props){
     super(props);
+    this.pagecontext = getPageContext();
     this.state={
     }
+  
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+
+
   }
   render() {
     return (
--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,42 @@ import Login from '../components/LoginPage';
 import App from '../components/App'
 import "isomorphic-fetch"
 import TweetsPanels from '../components/TweetPanels'
+import getPageContext from '../src/getPageContext';
 
 
-const Index = (props) => (
-  <Layout>
-    <div>
-      <h1>Welcome to Dapp Twitter</h1>
-      <p>Check current Bitcoin rate</p>
-      <TweetsPanels />
-    </div>
-  </Layout>
-);
 
+
+class MyApp extends App  {
+  constructor() {
+    super();
+    this.pageContext = getPageContext();
+  }
+  
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+  render() {
+    return (
+    <Layout>
+      <div>
+        <h1>Welcome to Dapp Twitter</h1>
+        <p>Check current Bitcoin rate</p>
+        <TweetsPanels />
+      </div>
+    </Layout>
+    );
+  }
 
 // const Index = (props) => ( 
 //   <App />
 // );
 
-Index.getInitialProps = async function() {
+
+MyApp.getInitialProps = async function() {
   const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
   const data = await res.json();
 
@@ -30,4 +48,4 @@ Index.getInitialProps = async function() {
   };
 }
 
-export default Index;
+export default MyApp;
